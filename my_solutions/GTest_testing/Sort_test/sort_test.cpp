22f32c8f9118c83bd9c0dc96c57384c1c54b764e
PawelN93 <paweln1993@gmail.com>
2023-10-13 12:14:03 +0200

    sort_test with GTest solution with basic Test_P

--- my_solutions/GTest_testing/Sort_test/sort_test.cpp
+++ my_solutions/GTest_testing/Sort_test/sort_test.cpp
@@ -2,18 +2,60 @@
 #include <algorithm>
 #include <vector>
 
-TEST(SortTest, TestOnVector321){
-    std::vector v = {3, 2, 1};
-    std::sort(v.begin(), v.end());
-    std::vector expected = {1, 2, 3};
+struct SortOnVector : public testing::TestWithParam<std::vector<int>> {
+    std::vector<int> expected = {1, 2, 3};
 
+    void setDataForGreaterComp(){
+        expected = {3, 2, 1};
+    }
+};
+
+TEST_P(SortOnVector, GivenAVectorWhenSortIsCalledThenVectorIsSorted) {
+    //GIVEN
+    auto v = GetParam();
+
+    //WHEN
+    std::sort(v.begin(), v.end());
+
+    //THEN
     ASSERT_EQ(v, expected);
 }
 
-TEST(SortTest, TestOnVector123){
-    std::vector v = {1, 2, 3};
-    std::sort(v.begin(), v.end());
-    std::vector expected = {1, 2, 3};
+TEST_P(SortOnVector, GivenAVectorWhenSortIsCalledWithGreaterThenComparatorThenVectorIsSortedDescending) {
+    //GIVEN
+    auto v = GetParam();
+    setDataForGreaterComp();
 
+    //WHEN
+    std::sort(v.begin(), v.end(), std::greater<int>{});
+
+    //THEN
     ASSERT_EQ(v, expected);
 }
+
+INSTANTIATE_TEST_SUITE_P(ThreeElementsVector,
+                         SortOnVector,
+                         testing::Values(std::vector<int>{1, 2, 3},
+                                         std::vector<int>{1, 3, 2},
+                                         std::vector<int>{2, 1, 3},
+                                         std::vector<int>{2, 3, 1},
+                                         std::vector<int>{3, 1, 2},
+                                         std::vector<int>{3, 2, 1}));
+
+
+
+// TEST(SortTest, TestOnVector321){
+//     std::vector v = {3, 2, 1};
+//     std::sort(v.begin(), v.end());
+//     std::vector expected = {1, 2, 3};
+
+//     ASSERT_EQ(v, expected);
+// }
+
+// TEST(SortTest, TestOnVector123){
+//     std::vector v = {1, 2, 3};
+//     std::sort(v.begin(), v.end());
+//     std::vector expected = {1, 2, 3};
+
+//     ASSERT_EQ(v, expected);
+// }
